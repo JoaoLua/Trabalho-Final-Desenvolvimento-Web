8ffe958aae7334e0a49503a7be390bd80d7d84a1
João <79210049+JoaoLua@users.noreply.github.com>
2024-09-17 16:30:58 -0300

    Delete sistema de notas/src/estilos/script directory

--- sistema de notas/src/estilos/script/script.js
+++
@@ -1,8 +0,0 @@
-function abrirNovaNota() {
-    alert()
-    document.getElementById('criarNotaId').style.display = 'flex';
-}
-
-function fecharNovaNota() {
-    document.getElementById('criarNotaId').style.display = 'none';
-}
