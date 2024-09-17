e3e3eb61155e0d4088b1e7087abb33cdd6e7365b
João <79210049+JoaoLua@users.noreply.github.com>
2024-09-17 16:30:26 -0300

    Add files via upload

---
+++ sistema de notas/src/estilos/script/script.js
@@ -0,0 +1,8 @@
+function abrirNovaNota() {
+    alert()
+    document.getElementById('criarNotaId').style.display = 'flex';
+}
+
+function fecharNovaNota() {
+    document.getElementById('criarNotaId').style.display = 'none';
+}
